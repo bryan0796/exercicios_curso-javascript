6c3a353d4b2dcefb6f157cec7dd77185f1c0d297
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2022-11-25 12:27:32 -0300

    iteracao-array-exercicio

--- script-exercicio.js
+++ script-exercicio.js
@@ -1,17 +1,36 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
+const cursos = document.querySelectorAll('.curso');
+const arrayCursos = Array.from(cursos);
+const objetoCursos = arrayCursos.map(curso => {
+  const titulo = curso.querySelector('h1').innerText;
+  const descricao = curso.querySelector('p').innerText;
+  const aulas = curso.querySelector('.aulas').innerText;
+  const horas = curso.querySelector('.horas').innerText;
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas,
+  }
+})
+
+console.log(objetoCursos)
 
 
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
-
+const maiorq100 = numeros.filter((numero) => numero > 100)
+maiorq100.sort();
+console.log(maiorq100);
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
 const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
-
+const possuiBaixo = instrumentos.includes('Baixo');
+console.log(possuiBaixo);
 
 // Retorne o valor total das compras
 const compras = [
@@ -36,3 +55,9 @@ const compras = [
     preco: 'R$ 10,60'
   }
 ]
+const valorTotal = compras.reduce((acumulador, compra) => {
+  const precoLimpo = +compra.preco.replace('R$ ', '').replace(',', '.');
+  return acumulador + precoLimpo;
+},0)
+
+console.log(valorTotal);
